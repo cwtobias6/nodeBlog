57be690e2efcfe659bd952f3db70b4bc929e984b
Christian Tobias <cwtobias6@gmail.com>
2016-01-26 10:47:24 -0600

    added show page route and html

--- app.js
+++ app.js
@@ -51,6 +51,17 @@ app.post("/blogs", function(req, res) {
 	});
 });
 
+// Show Route
+app.get("/blogs/:id", function(req,res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog:foundBlog});
+		}
+	});
+})
+
 
 
 
