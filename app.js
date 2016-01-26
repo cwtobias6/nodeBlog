d014d6d951b8d5bfb4f71d371829a68ddfe204ec
Christian Tobias <cwtobias6@gmail.com>
2016-01-26 09:35:32 -0600

    created index route

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/blog");
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+
+app.get("/", function(req, res) {
+	res.redirect("/blogs");
+})
+
+app.get("/blogs", function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		if(err) {
+			console.log("Error");
+		} else {
+			res.render("index", {blogs: blogs});	
+		}
+	})
+});
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('The Server has begun!!');
+});
