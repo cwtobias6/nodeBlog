aecece685baca00f26806a0e28bcb10b903a0325
Christian Tobias <cwtobias6@gmail.com>
2016-01-26 09:11:18 -0600

    more inital set-up

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/blog");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
