08c2e2d48a9e99fa55e2602bc3585d8935bf9cef
Christian Tobias <cwtobias6@gmail.com>
2016-01-26 11:46:58 -0600

    put request updated

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
 	title: String,
@@ -72,6 +74,17 @@ app.get("/blogs/:id/edit", function(req,res) {
 		}
 	});
 });
+
+// Update Route
+app.put("/blogs/:id", function(req,res) {
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+})
 
 
 
